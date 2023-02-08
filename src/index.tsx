8ad177819c3a16d18d8b0f7119e06154cfec2e34
Crash2022 <crash555@mail.ru>
2023-02-08 23:40:53 +0500

    add: node modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {HashRouter} from 'react-router-dom'
 
-import { App } from './app/App'
-import { store } from './app/store'
+import {App} from './app/App'
+import {store} from './app/store'
 import reportWebVitals from './reportWebVitals'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <HashRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </HashRouter>
+    <HashRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </HashRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
